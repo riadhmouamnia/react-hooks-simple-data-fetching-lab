70df4338255eafc3b0704d4e6d97817be88cd12c
Riadh Mouamnia <riadh.mouamnia@gmail.com>
2023-09-11 17:14:56 +0100

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
-// create your App component here
+import React, { useEffect, useState } from "react";
+
+const URL = "https://dog.ceo/api/breeds/image/random";
+
+function App() {
+  const [dog, setDog] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+
+  async function getRandomDog(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+    setLoaded(true);
+    setDog(data.message);
+  }
+
+  useEffect(() => {
+    getRandomDog(URL);
+  }, []);
+  return (
+    <main>
+      {!loaded && <p>Loading...</p>}
+      {loaded && <img src={dog} alt="A Random Dog" />}
+    </main>
+  );
+}
+
+export default App;
